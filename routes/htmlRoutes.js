3b3eebaef4200cd1fc123057f81cad4400b1bbe9
Matt DeMonaco <mdemonaco@gmail.com>
2020-04-06 18:35:13 -0700

    removing some nav bar items. Adding routing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,20 @@ var path = require('path');
 
 module.exports = function(app) {
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
+    res.sendFile(path.join(__dirname, "../public/index"))
+});
+
+
+app.get("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/contact"))
+});
+
+app.get("/portfolio", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/portfolio"))
+});
+
+app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/about"))
 });
 
 }
