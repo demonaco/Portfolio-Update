5f19d990719dc456792454a95d9317cbb0d0336f
Matt DeMonaco <mdemonaco@gmail.com>
2020-04-16 22:47:16 -0700

    adding replacing forward slashes with backwards slashes. adding name property to name input

--- public/contactform.php
+++ public/contactform.php
@@ -1,13 +1,13 @@
 <?php
 
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
 
     $mailTo = "mdemonaco@gmail.com";
     $headers = "From: ".$email;
-    $txt = "You have received an email from ".$name."./n/n".$message;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
 
     mail($mailTo, $txt, $headers);
     header("Location: index.php?mailsend");
