4f52c5cf297255dad5ae63f69fb8f0247564296d
Matt DeMonaco <mdemonaco@gmail.com>
2020-04-27 23:40:33 -0700

    adding post request to send contact form content to my email

---
+++ public/contactForm.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $("#submit").on("click", function(e) {
+        e.preventDefault();
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var input = $("#inputForm").val();
+        console.log(name, email, input);
+
+        if (name === "" || email === "" || input === "") {
+            console.log("no email, name, or message");
+            return false;
+        } else {
+            console.log("all good");
+            var newMessage = {
+                name: name,
+                email: email,
+                input: input
+
+            }
+            $.post("/", newMessage).then(function(){
+                window.location.href = "mailto:mdemonaco@gmail.com?" + "subject=" + name + "&body=" + input;
+            });
+        };
+    });
+});
